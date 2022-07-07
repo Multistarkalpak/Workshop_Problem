7809a791c34fe5d536b80526d946893d17646a13
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-07-07 09:30:37 +0530

    Finding power of two

---
+++ Workshop__/Workshop__/Power_of_two.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop
+{
+    internal class Powerof2
+    {
+        public void Poweroftwo()
+        {
+            Console.WriteLine("Enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            while (number < 0 || number > 31)
+            {
+                Console.WriteLine("Enter the valid number");
+            }
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+
+        }
+    }
+}
---
+++ Workshop__/Workshop__/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Workshop
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Powerof2 prg = new Powerof2();
+            prg.Poweroftwo();
+
+        }
+    }
+}
